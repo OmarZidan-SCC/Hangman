6396a144dbe3b19ade94530c80764fd82297875e
Omar Zidan <w0878888@myscc.ca>
2025-09-26 08:26:04 -0400

    Multiple topics added.. Moving forward to the while statement

--- src/HangmanMultiple.java
+++ src/HangmanMultiple.java
@@ -4,6 +4,11 @@ public class HangmanMultiple {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        // Three topics with different word lists
+        String[] topic1 = {"java", "code", "keyboard"};
+        String[] topic2 = {"apple", "banana", "orange"};
+        String[] topic3 = {"dog", "cat", "rabbit"};
+
 
     }
 }
