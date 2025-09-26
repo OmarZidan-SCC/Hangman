b0660b170f53f4eb376bb3c1a97d5fc3e1852e1b
Omar Zidan <w0878888@myscc.ca>
2025-09-26 08:32:27 -0400

    Lab 2 is done

--- src/HangmanMultiple.java
+++ src/HangmanMultiple.java
@@ -9,6 +9,76 @@ public class HangmanMultiple {
         String[] topic2 = {"apple", "banana", "orange"};
         String[] topic3 = {"dog", "cat", "rabbit"};
 
+        boolean playAgain = true;
 
+        while (playAgain) {
+            System.out.println("Select a topic:");
+            System.out.println("1: Programming");
+            System.out.println("2: Fruits");
+            System.out.println("3: Animals");
+            System.out.print("Enter 1, 2, or 3: ");
+
+            int choice = input.nextInt();
+            String[] words;
+
+            if (choice == 1) {
+                words = topic1;
+            } else if (choice == 2) {
+                words = topic2;
+            } else if (choice == 3) {
+                words = topic3;
+            } else {
+                System.out.println("Invalid choice, defaulting to Programming");
+                words = topic1;
+            }
+
+            // Randomly pick a word
+            int index = (int) (Math.random() * words.length);
+            String word = words[index];
+
+            // Hidden word
+            String hidden = "";
+            for (int i = 0; i < word.length(); i++) {
+                hidden += "*";
+            }
+
+            int misses = 0;
+
+            while (hidden.contains("*")) {
+                System.out.print("(Guess) Enter a letter in word " + hidden + " > ");
+                String guess = input.next().toLowerCase();
+
+                if (hidden.contains(guess)) {
+                    System.out.println(guess + " is already in the word");
+                    continue;
+                }
+
+                boolean correct = false;
+                String newHidden = "";
+                for (int i = 0; i < word.length(); i++) {
+                    if (word.charAt(i) == guess.charAt(0)) {
+                        newHidden += guess.charAt(0);
+                        correct = true;
+                    } else {
+                        newHidden += hidden.charAt(i);
+                    }
+                }
+                hidden = newHidden;
+
+                if (!correct) {
+                    System.out.println(guess + " is not in the word");
+                    misses++;
+                }
+            }
+
+            System.out.println("The word is " + word + ". You missed " + misses + " time(s).");
+
+            System.out.print("Do you want to guess another word? Enter Y or N: ");
+            String answer = input.next();
+            if (!answer.equalsIgnoreCase("Y")) {
+                playAgain = false;
+                System.out.println("Thanks for playing!");
+            }
+        }
     }
 }
